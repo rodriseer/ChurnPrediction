29f467aa284139ddce9f7f8bbbb652c01c89367e
Rodrigo Seer <109327635+rodriseer@users.noreply.github.com>
2024-09-27 08:48:51 -0400

    Add files via upload
    Added:
    - visualziation with matplotlib, extracting data with pandas

---
+++ data_visuals.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
+import time
+
+# connecting to the sql db via sqlalchemy instead of mysql conn, due to pandas conflict
+engine = create_engine('mysql+pymysql://root:47Nbm|}4B11=@localhost/churn_db')
+
+interactions = pd.read_sql_query("SELECT * FROM interactions", engine)
+
+class DataVisualization:
+    def __init__(self):
+
+        self.basic = 0
+        self.pro = 0
+        self.premium = 0
+        
+    def customer_counter(self):
+        """
+        method to pull total # of customers from the table
+        
+        """
+        customer_count_df = pd.read_sql_query("SELECT COUNT(*) as total_customers FROM customers", engine)
+        # extracting numbers
+        # starting from table 0 through iloc[0]
+        customer_count = customer_count_df['total_customers'].iloc[0]
+        return customer_count
+    
+    def subs_counter(self):
+        """
+        method to pull subscriptions vs non subscriptions data
+        """
+        subs_count_df = pd.read_sql_query("SELECT COUNT(*) as total_subs FROM subscriptions", engine)
+        subs_count = subs_count_df['total_subs'].iloc[0]
+
+        # count how many subs have 'basic' as plan_type
+        basic_count_df = pd.read_sql_query("SELECT COUNT(*) as basic_subs FROM subscriptions WHERE plan_type = 'basic'", engine)
+        basic_subs = basic_count_df['basic_subs'].iloc[0]
+
+        # count how many subs have 'pro'
+        pro_count_df = pd.read_sql_query("SELECT COUNT(*) as pro_subs FROM subscriptions WHERE plan_type = 'pro'", engine)
+        pro_subs = pro_count_df['pro_subs'].iloc[0]
+
+        # count how many subs are premium
+        premium_count_df = pd.read_sql_query("SELECT COUNT(*) as premium_subs FROM subscriptions WHERE plan_type = 'premium'", engine)
+        premium_subs = premium_count_df['premium_subs'].iloc[0]
+
+        return subs_count, basic_subs, pro_subs, premium_subs
+
+    def visualizations(self):
+        # call method o get count
+        customer_count = self.customer_counter()
+        subs_count, basic_subs, pro_subs, premium_subs = self.subs_counter()
+        
+        # plot data visualizations for custoemers
+        plt.bar(['customers'], [customer_count])
+        plt.ylabel('number of customers')
+        plt.title('customer count')
+        plt.show()
+
+        # plot data visualizations for subscriptions
+        plt.bar(['subscriptions'], [subs_count])
+        plt.ylabel('number of susbs')
+        plt.title("subs count")
+        plt.show()
+
+        # pie chart for subs type
+        labels = ['basic', 'pro', 'premium']
+        sizes = [basic_subs, pro_subs, premium_subs]
+        plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+        plt.axis('equal')
+        plt.title('Subs Types')
+        plt.show()
+
+# instance of the class
+data_visu = DataVisualization()
+data_visu.visualizations()
--- populate_sql_script_with_faker.py
+++ populate_sql_script_with_faker.py
@@ -3,119 +3,125 @@ import mysql.connector
 from random import choice
 from datetime import date
 
-# initialize faker
+# initialize faker, using a seed so every time the faker is used the same is also generated
 fake = Faker()
+fake.seed_instance(9971)
 
 # connecting to the sql db
-"""
-host = mysql host
-user = usually root
-password = ...
-user = ...
-"""
 conn = mysql.connector.connect(
     host="localhost",        
     user="root",            
-    password="",
+    password="47Nbm|}4B11=",
     database="churn_db"
 )
-
 
 # cursor to the db
 """
 cursor to allow us to interact with the database
 so I can execute SQL commands like SELECT, INSERT, UPDATE
 also manage the result sets returned by those queries.
-
 """
 cursor = conn.cursor()
 
-# insert data into customer table
-"""
 
-iterate through customer_id first because that will tell how many customers are in the business
-description of loop: 1, 100. will create 100 instances of customers, this can be modified as will
-"""
-for customer_id in range(1, 101):
-    """
-    sign_up_date = randomly generated date between 2 years ago (-2y) and 1 year ago (-1y).
-    account_status = choice(['active', 'inactive']) works as active or inactive accounts
-    last_login = random genearate date between sign up date and today, logic here is that the custoemr logged in after signing up....
-    """
-    sign_up_date = fake.date_between(start_date='-2y', end_date='-1y')
-    account_status = choice(['active', 'inactive'])
-    last_login = fake.date_between(start_date = sign_up_date, end_date='today')
+class SQLDataInserter:
+    def __init__(self, conn, cursor):
+        # initialize class with database connection
+        self.conn = conn
+        self.cursor = cursor
 
-    # burn it!
-    cursor.execute("""
-        INSERT INTO customers (sign_up_date, account_status, last_login)
-        VALUES (%s, %s, %s)
-    """, (sign_up_date, account_status, last_login))
-# committing customers
-conn.commit()
-print("Customers table populated.")
+    def insert_customers(self, num_customers = 100):
+        # insert data into customer table
+        """
+        iterate through customer_id first because that will tell how many customers are in the business
+        description of loop: 1, 100. will create 100 instances of customers, this can be modified as will
+        """
+        for customer_id in range(1, num_customers +1):
+            """
+            sign_up_date = randomly generated date between 2 years ago (-2y) and 1 year ago (-1y).
+            account_status = choice(['active', 'inactive']) works as active or inactive accounts
+            last_login = random genearate date between sign up date and today, logic here is that the custoemr logged in after signing up....
+            """
+            sign_up_date = fake.date_between(start_date='-2y', end_date='-1y')
+            account_status = choice(['active', 'inactive'])
+            last_login = fake.date_between(start_date = sign_up_date, end_date='today')
 
-
-# insert data into subscriptions table
-"""
-iterate through subscrptions_id first because that will tell how many subss are in the business
-description of loop: 1, 100. will create 100 instances of subs, this can be modified as will
-"""
-for subscription_id in range(1, 101):
-    """
-    customer_id = using choice to random choose between 1, 100 customers that are subscriber
-    start_date = random date between 1 yer ago and today for when the subscrition ended
-    end_date = randomly generated end date between start_date and today, or None if the sub hasnt ended today
-    plan_type = randomly selected plan type ('basic', 'premium', or 'pro')
-    """
-    customer_id = choice(range(1,101))
-    start_date = fake.date_between(start_date='-1y', end_date = 'today')
-    end_date = fake.date_between(start_date=start_date, end_date = 'today') if choice([True, False]) else None
-    plan_type = choice(['basic', 'premium', 'pro'])
-
-    # burn it!
-    cursor.execute("""
-    INSERT INTO subscriptions (customer_id, start_date, end_date, plan_type)                   
-    VALUES (%s, %s, %s, %s)           
-""", (customer_id, start_date, end_date, plan_type))
-# subscriptions commiting
-conn.commit()
-print("Subs table populated.")
+            # burn it!
+            self.cursor.execute("""
+                INSERT INTO customers (sign_up_date, account_status, last_login)
+                VALUES (%s, %s, %s)
+            """, (sign_up_date, account_status, last_login))
+        # committing customers
+        self.conn.commit()
+        print(f"{num_customers} have been inserted!")
 
 
-"""
-interactions with the business
-in range of 1 to 500 interaction, can be changed at will
-"""
-for interaction_id in range(1, 501):
-    """
-    customer_id = random choice between 1, 100 - use your real number of customers for better precision
-    interaction_date = random date between 1 year ago and today
-    interaction_type = random "choice", what kind of interactions type?
-    details = fake details, 200 chars max, can be modified for chars
-    """
-    customer_id = choice(range(1, 101))
-    interaction_date = fake.date_between(start_date='-1y', end_date='today')
-    interaction_type = choice(['email', 'call', 'in_person'])
-    details = fake.text(max_nb_chars=200)
+    def insert_subs(self, num_subs = 100):
+        """
+        iterate through subscrptions_id first because that will tell how many subss are in the business
+        description of loop: 1, 100. will create 100 instances of subs, this can be modified as will
+        """
+        for subscription_id in range(1, num_subs+1):
+            """
+            customer_id = using choice to random choose between 1, 100 customers that are subscriber
+            start_date = random date between 1 yer ago and today for when the subscrition ended
+            end_date = randomly generated end date between start_date and today, or None if the sub hasnt ended today
+            plan_type = randomly selected plan type ('basic', 'premium', or 'pro')
+            """
+            customer_id = choice(range(1,101))
+            start_date = fake.date_between(start_date='-1y', end_date = 'today')
+            end_date = fake.date_between(start_date=start_date, end_date = 'today') if choice([True, False]) else None
+            plan_type = choice(['basic', 'premium', 'pro'])
 
-    cursor.execute("""
-        INSERT INTO interactions (customer_id, interaction_date, interaction_type, details)
-        VALUES (%s, %s, %s, %s)
-    """, (customer_id, interaction_date, interaction_type, details))
+            # burn it!
+            self.cursor.execute("""
+            INSERT INTO subscriptions (customer_id, start_date, end_date, plan_type)                   
+            VALUES (%s, %s, %s, %s)           
+        """, (customer_id, start_date, end_date, plan_type))
+        # subscriptions commiting
+        self.conn.commit()
+        print("Subs table populated.")
 
-# Commit all interactions at once
-conn.commit()
-print("Interactions table populated.")
 
+    def insert_interactions(self, num_interactions = 200):
+        """
+        interactions with the business
+        in range of 1 to 500 interaction, can be changed at will
+        """
+        for interaction_id in range(1, num_interactions +1):
+            """
+            customer_id = random choice between 1, 100 - use your real number of customers for better precision
+            interaction_date = random date between 1 year ago and today
+            interaction_type = random "choice", what kind of interactions type?
+            details = fake details, 200 chars max, can be modified for chars
+            """
+            customer_id = choice(range(1, 101))
+            interaction_date = fake.date_between(start_date='-1y', end_date='today')
+            interaction_type = choice(['email', 'call', 'in_person'])
+            details = fake.text(max_nb_chars=200)
 
+            self.cursor.execute("""
+                INSERT INTO interactions (customer_id, interaction_date, interaction_type, details)
+                VALUES (%s, %s, %s, %s)
+            """, (customer_id, interaction_date, interaction_type, details))
 
-"""
-commiting all changes,
-closing cursor,
-closing connection
-"""
-cursor.close()
-conn.close()
+        # Commit all interactions at once
+        self.conn.commit()
+        print("Interactions table populated.")
 
-print("Data inserted successfully!")
+    def close_connection(self):
+    
+        self.cursor.close()
+        self.conn.close()
+        print("Database connection closed.")
+
+# instance of the class
+inserter = SQLDataInserter(conn, cursor)
+
+# insert num of customers into the database
+inserter.insert_customers(100)
+inserter.insert_subs(100)
+inserter.insert_interactions(200)
+
+# close the connection after all data has been inserted
+inserter.close_connection()
